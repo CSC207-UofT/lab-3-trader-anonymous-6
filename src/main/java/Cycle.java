9993c79c2a87915dbffd03ac93817f81143db36d
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-09-27 20:06:50 -0400

    Done the required changes and made files Avnish-Pasari, pasariav 2

---
+++ src/main/java/Cycle.java
@@ -0,0 +1,36 @@
+public class Cycle implements Drivable, Tradable{
+
+    String company;
+    String type;
+    int maxSpeed;
+
+    public void Cycle(String company, String type, int maxSpeed)
+    {
+        this.company = company;
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public void upgradeSpeed()
+    {
+        this.maxSpeed++;
+    }
+
+    @Override
+    public void downgradeSpeed()
+    {
+        this.maxSpeed--;
+    }
+
+    @Override
+    public int getMaxSpeed()
+    {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public int getPrice() {
+        return 5000 * this.company.length * this.maxSpeed;
+    }
+}
---
+++ src/test/java/CycleTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+    public class CycleTest {
+        Cycle c;
+
+        @Before
+        public void setUp() throws Exception {
+            c = new Cycle();
+        }
+
+        @Test(timeout = 50)
+        public void TestGetMaxSpeed() {
+            assertEquals(1, c.getMaxSpeed());
+        }
+
+        @Test(timeout = 50)
+        public void TestUpgradeSpeed() {
+            c.upgradeSpeed();
+            assertEquals(2, c.getMaxSpeed());
+        }
+
+        @Test(timeout = 50)
+        public void TestDowngradeSpeed() {
+            c.downgradeSpeed();
+            assertEquals(0, c.getMaxSpeed());
+        }
+
+        @Test(timeout = 50)
+        public void TestGetPrice() {
+            assertEquals(10, c.getPrice());
+        }
+
+    }
+
